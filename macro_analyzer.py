bef6d397617a3ff121e1429726de3162ddfe028a
ashitoshjoshi45 <167453476+ashitoshjoshi45@users.noreply.github.com>
2026-04-19 23:42:51 +0530

    [Analysis] Implement suspicious API detection (Shell,Run,etc.) for Macros

--- macro_analyzer.py
+++ macro_analyzer.py
@@ -1,15 +1,32 @@
+import os
+# I used oletools as it's th eindustry standard for soc analysis
+from oletools.olevba import VBA_Parser
+
 def analyze_attachment(file_path):
+    
+    # Attempts to extract VBA macro source code from a given file and analyze it for potential malicious behavior.
+    
+    if not os.path.exists(file_path):
+        print(f"[!] File not found: {file_path}")
+        return None
+     
+    # Step 1: parse the file for OLE streams.
+    try:
+        parser = VBA_Parser(file_path)
+        if parser.detect_macros():
+            print(f"[+] Macros detected in {os.path.basename(file_path)}")
+    # Step 2: Detect if VBA Macros are embedded.
+        else:
+            print(f"[-] No macros found in {os.path.basename(file_path)}")
+            return None
+    
+    except Exception as e:
+        print(f"[!] Error Parsing file: {e}")
+        return None
     """
-    Step 1: parse the file for OLE streams.
-    Step 2: Detect if VBA Macros are embedded.
     step 3. Ectrect code and check for suspecious API calls(Run, Shell, etc)
     Step 4: Return the result of the analysis.
     """
-
-    print(f"[*] Analyzing attachment: {file_path} for malicious macros..")
-    #Logic to be implemented 
-    pass
-
 if __name__ == "__main__":
     # Example usage
     sample_file = 'samples/suspicious_attachment.docm'  # Replace with your actual file path

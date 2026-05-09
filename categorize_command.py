6435a968296b75615f19450bc59b51e082a32ab5
ashitoshjoshi45 <167453476+ashitoshjoshi45@users.noreply.github.com>
2026-05-09 22:17:55 +0530

    Implement outer loop for MITRE technique categorization

--- categorize_command.py
+++ categorize_command.py
@@ -1,4 +1,24 @@
 # ADDED ON 07-05-2026
+# ADDED ON 09-05-2026
+def categorize_command(command_string):
+    # Iterate through the dictionary where:
+    # Key = MITRE Technique (e.g., 'Execution (T1204)')
+    # Value = List of keywords (e.g., ['chmod +', './'])
+    
+    MITRE_MAP = {
+        'Resource Development (T1587)': ['curl -O', 'wget ', 'Invoke-WebRequest'],
+        'Execution (T1204)': ['chmod +', './'],
+        'Discovery (T1033)': ['whoami', 'uname'],
+        'Impact (T1485)' : ['rm -rf /']
+                              
+    }
+
+    # Outer-loop to iterate throygh each technique and its keyword list
+    for technique, keywords in MITRE_MAP.items():
+        pass
+    
+    # If the loop finishes without a match
+    return "Unknown/General Activity"
 #  FUNCTION categorize_command(command_string):
 #     # Iterate through the dictionary where:
 #     # Key = MITRE Technique (e.g., 'Execution (T1204)')

bb81d1bd821bccbeacffbbf1b2daa3e6abc41739
ashitoshjoshi45 <167453476+ashitoshjoshi45@users.noreply.github.com>
2026-07-23 23:01:29 +0530

    pseudo code for url_defanger.py

---
+++ url_defanger.py
@@ -0,0 +1,13 @@
+#added on 23-07-2026
+
+# // title: url extraction and defanging
+# class solution:
+#     def extract_and_defang(email_lines):
+#         // Initialize list for neutralized links
+#         // OUTER LOOP: For each line in email_lines
+#             // INNER LOOP: For each regex match of a URL in the line
+#                 // Replace 'http' with 'hxxp'
+#                 // Replace '.' with '[.]'
+#                 // Add to neutralized links list
+#         // Return list of defanged URLs
+#     end function
